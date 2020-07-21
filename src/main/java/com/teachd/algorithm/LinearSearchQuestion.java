647206e3c879461ce7f4427fdb134148b040e310
teachd000000 <teachd000000@gmail.com>
2020-07-22 00:35:10 +0900

    linearSearch 파라미터 변경

--- src/main/java/com/teachd/algorithm/LinearSearchQuestion.java
+++ src/main/java/com/teachd/algorithm/LinearSearchQuestion.java
@@ -1,20 +1,14 @@
 package com.teachd.algorithm;
 
-import java.util.List;
-
 public class LinearSearchQuestion {
 
     private LinearSearchQuestion() {
     }
 
-    public static int linearSearch(final int element, final List<Integer> list) {
-        return list.indexOf(element);
-    }
-
-    public static int linearSearch2(final int element, final List<Integer> list) {
+    public static int linearSearch(final int element, final int[] array) {
         int result = -1;
-        for (int i = 0; i < list.size(); i++) {
-            if (list.get(i) != element) continue;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] != element) continue;
             result = i;
         }
         return result;
--- src/test/java/com/teachd/algorithm/LinearSearchQuestionTest.java
+++ src/test/java/com/teachd/algorithm/LinearSearchQuestionTest.java
@@ -2,29 +2,19 @@ package com.teachd.algorithm;
 
 import org.junit.Test;
 
-import java.util.List;
-
 import static com.teachd.algorithm.LinearSearchQuestion.linearSearch;
-import static com.teachd.algorithm.LinearSearchQuestion.linearSearch2;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class LinearSearchQuestionTest {
 
     @Test
     public void testLinearSearch() {
-        assertThat(linearSearch(2, List.of(2, 3, 5, 7, 11))).isEqualTo(0);
-        assertThat(linearSearch(0, List.of(2, 3, 5, 7, 11))).isEqualTo(-1);
-        assertThat(linearSearch(5, List.of(2, 3, 5, 7, 11))).isEqualTo(2);
-        assertThat(linearSearch(3, List.of(2, 3, 5, 7, 11))).isEqualTo(1);
-        assertThat(linearSearch(11, List.of(2, 3, 5, 7, 11))).isEqualTo(4);
-    }
+        final int[] array = {2, 3, 5, 7, 11};
 
-    @Test
-    public void testLinearSearch2() {
-        assertThat(linearSearch2(2, List.of(2, 3, 5, 7, 11))).isEqualTo(0);
-        assertThat(linearSearch2(0, List.of(2, 3, 5, 7, 11))).isEqualTo(-1);
-        assertThat(linearSearch2(5, List.of(2, 3, 5, 7, 11))).isEqualTo(2);
-        assertThat(linearSearch2(3, List.of(2, 3, 5, 7, 11))).isEqualTo(1);
-        assertThat(linearSearch2(11, List.of(2, 3, 5, 7, 11))).isEqualTo(4);
+        assertThat(linearSearch(2, array)).isEqualTo(0);
+        assertThat(linearSearch(0, array)).isEqualTo(-1);
+        assertThat(linearSearch(5, array)).isEqualTo(2);
+        assertThat(linearSearch(3, array)).isEqualTo(1);
+        assertThat(linearSearch(11, array)).isEqualTo(4);
     }
 }
